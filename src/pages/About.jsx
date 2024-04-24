818170665d4c1c98d0ec06d839cf5a835f41e0fe
Yahyobek1505 <yahyohonorinboyev0105@gmail.com>
2024-04-24 12:46:08 +0500

    done

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -25,18 +25,18 @@ function About() {
       <section className="align-element py-20 dark:bg-[#272935] dark:text-white h-[100vh]">
         <div className="flex flex-wrap gap-4 sm:gap-x-6 items-center justify-center">
           <h1 className="text-4xl font-bold leading-none tracking-tight sm:text-6xl">
-            {t("love")}
+          We love
           </h1>
           <div className="stats bg-primary ml-3 shadow-md px-4 py-3  rounded-2xl">
             <div className="stat">
               <div className="stat-title text-white text-primary-content text-4xl font-bold tracking-widest">
-                {t("comfy")}
+              comfy
               </div>
             </div>
           </div>
         </div>
         <p className="mt-6 text-lg leading-8 max-w-2xl mx-auto">
-          {t("aboutDesc")}
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore quae quam blanditiis vitae, dolor non eveniet ipsum voluptatibus, quia optio aut! Perferendis ipsa cumque ipsam nostrum reprehenderit ad illo sed officiis ea tempore! Similique eos minima sit porro, ratione aspernatur!
         </p>
       </section>
     </div>
